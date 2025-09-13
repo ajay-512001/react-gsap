7f187331d99f475e41677d47bace04b0ee57a91b
ajay-512001 <ajaygajjarkar512001@gmail.com>
2025-09-13 21:08:05 +0530

    animation of char and lines with timeline and from and to

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-        <div className='text-3xl text-indigo-300'>APP REACT</div>
-    </div>
+    <main>
+        <Navbar />
+        <Hero />
+        <div className='h-dvh'></div>
+    </main>
   )
 }
 
